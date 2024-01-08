564481b26f1a5f43723b1cc85f1ebc40fffb4487
hgctmy <hgctomo@gmail.com>
2024-01-09 01:57:19 +0900

    -

---
+++ control_difficulty.py
@@ -0,0 +1,15 @@
+class Question:
+    def __init__(self, text, score):
+        self.text = text
+        self.score = score
+
+
+class User:
+    def __init__(self):
+        self.scores = []
+
+    def add_scores(self, question):
+        self.scores.append(question.score)
+
+    def calc_average(self):
+        return sum(self.scores) / len(self.scores)
